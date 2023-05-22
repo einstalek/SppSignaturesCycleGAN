4d9d2c4b7a874263439def13e64f3569084c3e91
Alexander Arganaidi <arganaidi@phystech.edu>
2023-05-22 18:36:43 +0200

    Update train_test_split.py
    Fixes in file paths

--- train_test_split.py
+++ train_test_split.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import os.path as osp
 import sys
 import random
 from shutil import copyfile
@@ -8,10 +9,11 @@ if __name__ == "__main__":
     dataset_root = sys.argv[1]
 
     for fold in ('trainA', 'testA', 'trainB', 'testB'):
-        os.makedirs(f'/app/split/{fold}', exist_ok=True)
-
-    a_fps = glob.glob(f'{dataset_root}/full_forg/*.png')
-    b_fps = glob.glob(f'{dataset_root}/full_org/*.png')
+        os.makedirs(osp.join("/app", "split", fold), exist_ok=True)
+    
+    
+    a_fps = glob.glob(os.path.join(dataset_root, 'forg', '*.png'))
+    b_fps = glob.glob(os.path.join(dataset_root, 'org', '*.png'))
 
     for fps, sub in [(a_fps, 'A'), (b_fps, 'B')]:
         train_size = int(0.9 * len(fps))
@@ -19,6 +21,8 @@ if __name__ == "__main__":
         train_fps, test_fps = fps[:train_size], fps[train_size:]
 
         for fp in train_fps:
-            copyfile(fp, f'/app/split/train{sub}/{os.path.basename(fp)}')
+            destination_path = os.path.join('/app', 'split', 'train' + sub, os.path.basename(fp))
+            copyfile(fp, destination_path)
         for fp in test_fps:
-            copyfile(fp, f'/app/split/test{sub}/{os.path.basename(fp)}')
+            destination_path = os.path.join('/app', 'split', 'test' + sub, os.path.basename(fp))
+            copyfile(fp, destination_path)
